42bec10799d2f12ae94db2790a8fd61cd82e089a
Peppestra <giuseppestrancia88@gmail.com>
2021-07-16 18:34:32 +0200

    client

--- client/calcolatrice/src/app/app.component.ts
+++ client/calcolatrice/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Risultato } from './risultato';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calcolatrice';
+  numero1: number = 0;
+  numero2: number = 0;
+  risultato: Risultato = new Risultato();
 }
+
+constructor (private http: HttpClient) {}
+
+somma() {
+  let ox: Observable<Risultato> = this.http.post<Risultato>("http://localhost:8080/somma", this.numero1 , this.numero2);
+    ox.subscribe(r => this.risultato = r);
+  }
+
+differenza() {
+  let ox: Observable<Risultato> = this.http.post<Risultato>("http://localhost:8080/differenza", this.numero1 , this.numero2);
+    ox.subscribe(r => this.risultato = r);
+  }
+
+moltiplicazione() {
+  let ox: Observable<Risultato> = this.http.post<Risultato>("http://localhost:8080/moltiplicazione", this.numero1 , this.numero2);
+    ox.subscribe(r => this.risultato = r);
+  }
+
+divisione() {
+  let ox: Observable<Risultato> = this.http.post<Risultato>("http://localhost:8080/divisione", this.numero1 , this.numero2);
+    ox.subscribe(r => this.risultato = r);
+  }
---
+++ client/calcolatrice/src/app/risultato.ts
@@ -0,0 +1,3 @@
+export class Risultato {
+    risultato: number = 0;
+}
